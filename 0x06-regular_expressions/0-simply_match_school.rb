087badc5acbbd3e359f062a10023111a102aba91
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-06-27 11:34:20 +0200

    commit

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-# a Ruby script that accepts one argument and pass it to a regular expression matching method
-# The regular expression must match Holberton
-puts ARGV[0].scan(/Holberton/).join
+puts ARGV[0].scan(/School/).join
